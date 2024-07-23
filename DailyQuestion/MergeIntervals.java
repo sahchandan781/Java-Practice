8075e4869e7ec6ec2b89f84d6c3c698a147fe57f
Chandan kumar Sah <93442672+sahchandan781@users.noreply.github.com>
2024-07-23 23:05:12 +0530

    daily question 23-07

---
+++ DailyQuestion/MergeIntervals.java
@@ -0,0 +1,43 @@
+package DailyQuestion;
+
+import java.util.*;
+
+public class MergeIntervals {
+    public static int[][] merge(int[][] intervals) {
+
+        if( intervals.length <=1){
+            return intervals;
+        }
+
+        //sort by ascending  starting point
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+
+        List<int[]> result = new ArrayList<>();
+
+        int[] newInterval = intervals[0];
+        result.add(newInterval);
+
+        for( int[] interval: intervals){
+            if(interval[0] <= newInterval[1]){
+                // Overlaping intervals,
+                // update the end if needed
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            }
+            else{
+                // Disjoint intervals,
+                // add the new interval to the list
+                newInterval = interval;
+                result.add(newInterval);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+        
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+
+        merge(intervals);
+    }
+}

07f3440e694e3223bf7f59785f1895e7eda1e5cd
Chandan kumar Sah <93442672+sahchandan781@users.noreply.github.com>
2024-09-20 12:16:49 +0530

    Dsa gfg question

--- Day13Array/BestTimeToBuyAndSellTheStock.java
+++ Day13Array/BestTimeToBuyAndSellTheStock.java
@@ -1,5 +1,26 @@
 package Day13Array;
 
 public class BestTimeToBuyAndSellTheStock {
-    
+    static int maxProfit(int[] prices){
+        int minSoFar = prices[0];
+        int res = 0;
+
+        //  Update the minimum value seen so far
+        //  If we see smaller
+
+        for(int  i = 0; i< prices.length -1; i++){
+            minSoFar = Math.min(minSoFar , prices[i]);
+
+            // update result if we get more profit
+
+            res = Math.max(res, prices[i] - minSoFar);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 10, 1, 3, 6, 9, 2};
+        System.out.println(maxProfit(prices));
+    }
 }
